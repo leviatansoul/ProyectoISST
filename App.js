6d5382c7ba3d954926032ff743c20f276b00209e
Manuel <manueljpy@gmail.com>
2018-03-07 19:11:28 +0100

    Localizacion

--- App.js
+++ App.js
@@ -26,10 +26,40 @@ import ListPrueba from './app/components/ListPrueba'
 import PensamientosScreen from './app/components/PensamientosScreen'
 
 class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.actualizaPosicion = this.actualizaPosicion.bind(this);
+    this.state = {
+        latitude: 0,
+        longitude: 0,
+        error: null,
+    };
+}
+
+componentDidMount() {
+  
+this.actualizaPosicion();
+
+}
+
+  actualizaPosicion(){
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }
 
   render () {
     return (
-      <MyApp />
+      <MyApp screenProps={this.actualizaPosicion}/>
     )
   }
 }
--- app/components/PublicarScreen.js
+++ app/components/PublicarScreen.js
@@ -3,7 +3,27 @@ import { View, Text } from 'react-native'
 import { Icon, Button, Container, Header, Content, Left, Right, Body, Title } from 'native-base'
 
 class PublicarScreen extends Component {
+  
+  
+  
+  constructor(props) {
+    super(props);
+    this.actualizaPosicion = this.actualizaPosicion.bind(this);
+}
+
+
+componentDidMount() {
+  
+this.actualizaPosicion();
+
+}
+
+actualizaPosicion(){
+  this.props.screenProps();
+}
+
   render () {
+
     return (
       <Container>
         <Header>
