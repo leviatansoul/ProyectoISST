0dfee05e96658b3511e8bc567338cc3005f42261
Manuel <manueljpy@gmail.com>
2018-05-20 03:04:37 +0200

    Eject v1

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -29,7 +29,7 @@ let pensamientosGuardadosState = { data:[ {id: 0, date: "", idPens: 0, latitude:
 let footerState = {itemSelected:1, badgeHome:0};
 let nicknameState = {nickname: ""};
 
-let urlState = {url: "192.168.1.136:8080"};
+let urlState = {url: "192.168.1.58:8080"};
 let pensamientosLocState = { data:[ {id: 0, date: "", idPens: 0, latitude:40.3385100, longitude: -3.38045, topic: "", text: 'Mi primer pensamiento', autor: 'Mirella', likes: 0}]};
 
 
