670a92362695836fde0dce83ba3c56987e8c5144
Manuel <manueljpy@gmail.com>
2018-05-20 17:55:52 +0200

    Eject v6 link Image Picker

--- android/app/build/generated/source/r/debug/com/google/maps/android/R.java
+++ android/app/build/generated/source/r/debug/com/google/maps/android/R.java
@@ -142,9 +142,9 @@ public final class R {
         public static final int common_signin_button_text_long = 0x7f0c003a;
     }
     public static final class style {
-        public static final int amu_Bubble_TextAppearance_Dark = 0x7f0d0142;
-        public static final int amu_Bubble_TextAppearance_Light = 0x7f0d0143;
-        public static final int amu_ClusterIcon_TextAppearance = 0x7f0d0144;
+        public static final int amu_Bubble_TextAppearance_Dark = 0x7f0d0143;
+        public static final int amu_Bubble_TextAppearance_Light = 0x7f0d0144;
+        public static final int amu_ClusterIcon_TextAppearance = 0x7f0d0145;
     }
     public static final class styleable {
         public static final int[] LoadingImageView = { 0x7f020049, 0x7f02007b, 0x7f02007c };
--- android/app/src/main/java/com/proyectoisst/MainApplication.java
+++ android/app/src/main/java/com/proyectoisst/MainApplication.java
@@ -3,6 +3,7 @@ package com.proyectoisst;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new VectorIconsPackage(),
               new MapsPackage()
       );
